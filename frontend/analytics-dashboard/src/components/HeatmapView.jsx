280149b63c583c3ee549f2ef0b6955a87dcb3df3
ChanakyaKamuju <chanakyamecon@gmail.com>
2026-05-14 18:28:39 +0530

    Made Heatmap adaptive to larger display

--- frontend/analytics-dashboard/src/components/HeatmapView.jsx
+++ frontend/analytics-dashboard/src/components/HeatmapView.jsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-// const API_BASE =
-//   import.meta.env.VITE_API_BASE ||
-//   "https://useranalyticsplatform.onrender.com/api";
 const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
 
 const HeatmapView = () => {
